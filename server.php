bb5d318abea047f500d6f90206fe268e73555819
xhq <1434970057@qq.com>
2016-04-03 22:06:04 +0800

    jfk

--- server.php
+++ server.php
@@ -9,8 +9,12 @@ include BASEDIR.'/Sunphp/Loader.php';
 *注册类自动加载方法
 */
 spl_autoload_register('\\Sunphp\\Loader::autoloadfff');
-$i = 1;
-while ($i--) {
-\SeasLog::debug('this is a  debug',array('jhg' => 'neeke'));
+try{
+	$i = 1;
+	while ($i--) {
+	\SeasLog::debug('this is a  debug',array('jhg' => 'neeke'));
+	}
+	\Lib\Vendor\SeasLog\Log::debug('11111111122223');
+}catch(Exception $e){
+	\SeasLog::error($e->getMessage());
 }
-\Lib\Vendor\SeasLog\Log::debug('11111111122223');
