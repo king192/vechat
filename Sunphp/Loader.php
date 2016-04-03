7afadc1292c04e7ad306f66df629b283fdc9a22b
xhq <1434970057@qq.com>
2016-04-03 22:19:48 +0800

    jfk

--- Sunphp/Loader.php
+++ Sunphp/Loader.php
@@ -7,7 +7,7 @@ class Loader{
 	*/
 	static function autoloadfff($class){
 		// echo 'c='.$class;
-		$path = BASEDIR.'/'.$class.'.php';
+		$path = BASEDIR.'/'.$class.'.class.php';
 		// echo 'p='.$path;
 		require($path);
 	}
