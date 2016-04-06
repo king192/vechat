c554e0d1944b2f39418e3d523bd2644fe549d941
xhq <1434970057@qq.com>
2016-04-06 12:35:53 +0800

    bushu

--- Common/Conf/vewp.suoga.org.php
+++ Common/Conf/vewp.suoga.org.php
@@ -2,7 +2,7 @@
 return array(
 	//测试号
 	'vechatapp'=>array(
-		'appid'=>'wxb39afc8e3bd62749',
-		'appsecret'=>'8ff4cdfe5cee4b03fc2a88604a0358d6',
+		'appid'=>'wx3a5aac7161b28013',
+		'appsecret'=>'d4624c36b6795d1d99dcf0547af5443d',
 	),
 );
