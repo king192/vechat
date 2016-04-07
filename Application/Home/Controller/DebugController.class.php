d3175267cb5b8405853454677987cddf0a605611
xhq <1434970057@qq.com>
2016-04-07 16:41:56 +0800

    vechat_api

--- Application/Home/Controller/DebugController.class.php
+++ Application/Home/Controller/DebugController.class.php
@@ -3,7 +3,8 @@ use Sunphp\Lib\Vechat\vecommon;
 class DebugController {
 	public function index(){
 		vecommon::init_config();
-		vecommon::get_test();
+		// vecommon::get_test();
+		echo TOKEN;
 	}
 	public function index1(){
 		vecommon::debug();
