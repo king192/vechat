897d2511a201075e31824fd0f3bf084234b1148a
xhq <1434970057@qq.com>
2016-04-07 18:46:13 +0800

    vechat_api

--- Sunphp/Lib/Vechat/velogin.class.php
+++ Sunphp/Lib/Vechat/velogin.class.php
@@ -10,18 +10,18 @@ class velogin {
 	  }
 	/*我微信oauth2认证*/
 	static public function oauth2(){
-		$param = '';
-		if(!empty($_GET['param'])){
-		    $param .= "?param=".$_GET['param'];
-		}
-		if(!empty($_GET['bid'])){
-		    $param .= "?bid=".$_GET['bid'];
-		} 
-		if(!empty($_GET['auto_puid'])){
-		    $param .= "?auto_puid=".$_GET['auto_puid'];
-		} 
+		// $param = '';
+		// if(!empty($_GET['param'])){
+		//     $param .= "?param=".$_GET['param'];
+		// }
+		// if(!empty($_GET['bid'])){
+		//     $param .= "?bid=".$_GET['bid'];
+		// } 
+		// if(!empty($_GET['auto_puid'])){
+		//     $param .= "?auto_puid=".$_GET['auto_puid'];
+		// } 
 		$appid = VECHAT_APPID;  //公众号的唯一标识
-		$redirect_uri = urlencode("http://".$_SERVER['SERVER_NAME']."/Home/Index/getuserinfo".$param);
+		$redirect_uri = urlencode("http://".$_SERVER['SERVER_NAME']."/Home/Index/getuserinfo");
 		$state = 'other';
 		$is_scope = max(0,$_GET['is_scope']);
 
@@ -30,7 +30,8 @@ class velogin {
 		// if($is_scope>0){
 		if(!isset($_GET['is_scope'])){
 		    // 一般模式
-		    $url .= '?appid='.$appid.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state='.$state.'#wechat_redirect';
+		    // $url .= '?appid='.$appid.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state='.$state.'#wechat_redirect';
+		    $url .= '/appid/'.$appid.'/redirect_uri/'.$redirect_uri.'/response_type/code/scope/snsapi_userinfo/state/'.$state.'#wechat_redirect';
 		}else{
 		    // 静默模式
 		    // echo 'hello world';
